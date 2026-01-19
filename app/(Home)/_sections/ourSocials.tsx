f50563c00d45c068ca8b74db6f73b4b62cd9c939
Felipe <oliveira8.1felipe@gmail.com>
2026-01-19 19:01:30 -0300

    videos added, now figuring spacing between sections out

--- app/(Home)/_sections/ourSocials.tsx
+++ app/(Home)/_sections/ourSocials.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 const OurSocials = () => {
     return (
-        <section className="Banner6 flex-wrap flex-row flex relative justify-end items-center bg-[#F5EBD0] w-full h-[90vh] pt-32">
+        <section className="Banner6 flex-wrap flex-row flex relative justify-end items-center bg-[#F5EBD0] w-full h-[90vh] pt-30">
             <div className="banner6 flex justify-center items-center w-full h-[30vh]">
                 <Hero dropShadow="drop-shadow-[-4px_6px_0px_rgba(73,130,157,1)]" text="Our Socials" color="" width="w-[95%]" text_align="text-center" />
             </div>
-            <div className="flex justify-center items-center w-full h-[30vh]">
+            <div className="flex justify-center items-center w-full">
                 <Image src="/svg/linkedin.svg" alt="LinkedIn" width ={50} height = {50}/>
                 <Image src="/svg/instagram.svg" alt="Instagram" width ={50} height = {50}/>
                 <Image src="/svg/youtube.svg" alt="YouTube" width ={50} height = {50}/>
--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -39,7 +39,7 @@ const MenuLink = ({ text }: { text: string }) => {
 const Footer = () => {
 
     return (
-        <section className="Footer flex-wrap flex-row flex relative justify-end items-center bg-[#F5EBD0] w-full h-[80vh]">
+        <section className="Footer flex-wrap flex-row flex relative justify-end items-center bg-[#F5EBD0] w-full h-[90vh]">
             <div className="flex flex-col justify-center items-center w-full h-[30vh]">
                 <Hero dropShadow="drop-shadow-[-4px_6px_0px_rgba(218,157,43,1)]" text="Hermanos Gutierrez" color="" width="w-full" text_align="text-center"/>
                 <h2 className="font-paquito text-3xl">Copyright 2026</h2>
